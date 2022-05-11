2e18a0baa79b07e9f3f1d6003c3f2928863cdf81
trueLoving <starsky.zhanglinjie@gmail.com>
2022-05-11 23:15:30 +0800

    ✈️: flying

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-console.log("Hello Playground !!!! 😆")
+const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(3)
+  }, 3000)
+})
 
-console.log("Hello Playground again !!!! 😆")
+p.then(_ => {
+  console.log('resolved')
+}).catch(_ => {
+  console.log('rejected')
+}).finally(() => {
+  console.log('finally')
+})
+
+// output
+// resolved
+// finally
